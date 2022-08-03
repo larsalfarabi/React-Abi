160af1c593af7cf1f9cd9db20ed35979c9d1e031
larsalfarabi <msyamil404@gmail.com>
2022-08-03 14:31:59 +0700

    belajar state

--- src/App.js
+++ src/App.js
@@ -5,36 +5,57 @@ import Layout from "./komponen/layout";
 import Button from "./komponen/button";
 import "./style/style.css";
 
-function App() {
-  return (
-    <React.Fragment>
-      <h1> belajar Props 2 </h1>{" "}
-      <Layout title={"Nomor 1"}>
-        <h1> SMK MADINATUL QURAN </h1>{" "}
-      </Layout>{" "}
-      <Layout title={"Nomor 2"}>
-        <h1> SMK IDN </h1>{" "}
-      </Layout>{" "}
-      <Layout title={"Nomor 3"}>
-        <h1> SMKN 1 CIBINONG </h1>{" "}
-      </Layout>{" "}
-      <Button
-        onClick={() => {
-          console.log("Button simpan diklik");
-        }}
-        color="blue"
-        title={"Simpan"}
-      />{" "}
-      <Button
-        onClick={() => {
-          console.log("Button batal diklik");
-        }}
-        disabled={true}
-        title={"Batal"}
-      />{" "}
-      <Button color="green" title={"Update"} />{" "}
-    </React.Fragment>
-  );
+export default function App(){
+
+    let [name,setName]= React.useState('');
+    let [email,setEmail]= React.useState('');
+    let [password,setPassword]= React.useState('');
+    let [confirmPassword,setConfirmPassword]= React.useState('');
+    return(
+        <React.Fragment>
+            <form>test</form>
+        </React.Fragment>
+    )
 }
 
-export default App;
+
+
+
+
+
+
+
+
+// function App() {
+//   let [count, setCount] = React.useState(0);
+//   const handleTambah = () => {
+//     setCount(count + 1);
+//   };
+//   const handleKurang = () => {
+//     setCount(count - 1);
+//   };
+//   const handleResets = () => {
+//     setCount(0);
+//   };
+
+//   return (
+//     <React.Fragment>
+//       <h1>Count = {count}</h1>
+//       <Button onClick={handleTambah} title="Tambah" color="blue" />
+//       <Button
+//         disabled={count <= 0 ? true : false}
+//         onClick={handleKurang}
+//         title="Kurang"
+//         color="blue"
+//       />
+//       <Button
+//         disabled={count === 0 ? true : false}
+//         onClick={handleResets}
+//         title="Resets"
+//       />
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
+
--- src/komponen/button.jsx
+++ src/komponen/button.jsx
@@ -26,13 +26,15 @@ import React from "react";
 //   );
 // }
 
-export default function Button({ title, color = "red", ...props }) {
+export default function Button({ title, color = "red", disabled,...props }) {
   return (
     <React.Fragment>
       <button
+        disabled={disabled}
         {...props}
         style={{
           backgroundColor: color,
+          opacity: disabled ? 0.5 : 1
         }}
         className="button"
       >

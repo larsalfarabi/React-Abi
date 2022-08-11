7774da930e27be76e6e7f3e6fcca24ff93a33c86
larsalfarabi <msyamil404@gmail.com>
2022-08-11 14:29:11 +0700

    belajar useEffect

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-// import logo from './logo.svg';
-
+import "./style/style.css";
 import React from "react";
 
+export default function App() {
+  let [message, setMessage] = React.useState(0);
+  let [text, setText] = React.useState(true);
+  let [count, setCount] = React.useState(0);
+  let [loading, setLoading] = React.useState(true)
+  React.useEffect(() => {
+    setMessage(message + 1);
+    console.log("use effect berjalan");
+  }, [count, text]);
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Hello World
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+  React.useEffect(() => {
+    setTimeout(()=>{
+      setLoading(false);
+    }, 1000)
+  }, [])
 
+  if(loading){
+    return <h1>Loading...</h1>;
+  }
+
+  console.log(text);
+  return (
+    <React.Fragment>
+      <h1>Belajar Use Effect</h1>
+      <h1>Message: {message}</h1>
+      <h1>Count: {count}</h1>
+      <h3>{message === 10 ? 'ini sepuluh' : "bukan sepuluh"}</h3>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Tambah
+      </button>
+      <button
+        onClick={() => {
+          setText(!text);
+        }}
+      >
+        Ubah
+      </button>
+    </React.Fragment>
+  );
+}

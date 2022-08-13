596655e14b132dfcbbd160c0a962250d3687a74e
larsalfarabi <msyamil404@gmail.com>
2022-08-13 08:22:11 +0700

    ujian

--- src/App.js
+++ src/App.js
@@ -4,11 +4,4 @@ import React from "react";
 import "./style/style.css"
 
 
-export default function App() {
-  return (
-    <React.Fragment>
-      <h1>Branch Main</h1>
-    </React.Fragment>
-  );
-}
 

d5a5e01be741aa9651b14da92b3831773fa78e18
larsalfarabi <msyamil404@gmail.com>
2022-08-28 10:41:40 +0700

    javaScriptMastery3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-import React from "react";
 import styles from "./style";
 import {
-  Navbar,
-  Hero,
-  Stats,
-  Business,
   Billing,
+  Business,
   CardDeal,
-  Testimonials,
   Clients,
   CTA,
   Footer,
+  Navbar,
+  Stats,
+  Testimonials,
+  Hero,
 } from "./components";
 
 const App = () => (
-  <div className="bg-black w-full overflow-hidden">
-    <div className={`${styles.paddingX}${styles.flexCenter}`}>
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`bg-black ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats />
         <Business />
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { clients } from "../constans/index";
+import styles from "../style";
 
-const Client = () => {
-  return (
-    <div>Client</div>
-  )
-}
+const Clients = () => (
+  <section className={`${styles.flexCenter} my-4`}>
+    <div className={`${styles.flexCenter} flex-wrap w-full`}>
+      {clients.map((client) => (
+        <div
+          key={client.id}
+          className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}
+        >
+          <img
+            src={client.logo}
+            alt="client_logo"
+            className="sm:w-[192px] w-[100px] object-contain"
+          />
+        </div>
+      ))}
+    </div>
+  </section>
+);
 
-export default Client
+export default Clients;

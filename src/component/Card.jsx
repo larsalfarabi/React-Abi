47deb4a5b4913c33377d95f82b916535dd03b804
larsalfarabi <msyamil404@gmail.com>
2022-08-31 13:04:23 +0700

    tahun penerbit

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,13 +1,14 @@
 import Button from "./Button";
 import { showFormattedDate } from "../utils";
-export default function Card({ title, body, createdAt, id, handleDelete }) {
+export default function Card({ title, body, date,createdAt, id, handleDelete }) {
   return (
     <div className="border p-5 h-72 w-full shadow-lg rounded-lg space-y-2">
       <div className="h-1/5">
         <h5 className="text-xl uppercase">{title}</h5>
         <p className="text-xs italic">{showFormattedDate(createdAt)}</p>
       </div>
-      <div className="text-justify h-3/5">{body}</div>
+      <div className="text-justify h-3/6">{body}</div>
+      <h5 className="text-m">{date}</h5>
 
       <div className="grid grid-cols-2 gap-5">
         <Button onClick={handleDelete} value={id} type="button" title={"Delete"} bg="red" />

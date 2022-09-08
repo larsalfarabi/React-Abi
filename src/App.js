880fccbcccc8fb2e05e58610faf9e841ecc1680f
larsalfarabi <msyamil404@gmail.com>
2022-09-08 11:13:44 +0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-import Input from "./component/Input";
-import TextArea from "./component/TextArea";
-import Button from "./component/Button";
+// import Input from "./component/Input";
+// import TextArea from "./component/TextArea";
+// import Button from "./component/Button";
 
-import Card from "./component/Card";
+// import Card from "./component/Card";
 export default function App() {
   return (
     <div>

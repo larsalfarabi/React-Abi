0c03145c6e0b54c463f5b5520dfc485b7f2f12df
larsalfarabi <msyamil404@gmail.com>
2022-09-25 09:55:37 +0700

    master

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-import Input from "./component/Input";
-import TextArea from "./component/TextArea";
-import Button from "./component/Button";
 
-import Card from "./component/Card";
 export default function App() {
   return (
     <div>

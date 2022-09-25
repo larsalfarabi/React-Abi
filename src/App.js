a44af0ca333c263c7e69141b0c90ee29428a4ec7
larsalfarabi <msyamil404@gmail.com>
2022-09-25 13:57:10 +0700

    material UI

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1> branch master</h1>
+      <h1></h1>
     </div>
   )
 
---
+++ src/Library/Material.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MaterialUi = () => {
+  return <div>Latihan Material UI</div>;
+};
+
+export default MaterialUi;

7f6d699146b017161ab42c4eb9fc98601702c5c1
larsalfarabi <msyamil404@gmail.com>
2022-11-24 10:56:22 +0700

    visimisi

---
+++ src/component/About.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="flex flex-row">
+      <div>
+        <h1>Vision</h1>
+        <p>
+          To become the leading academic-based English training center in
+          Indonesia
+        </p>
+      </div>
+      <div>
+        <h1>Mision</h1>
+        <p>
+          • Creating a comfortable and fun academic English learning
+          environment.<br />• Organizing an effective and efficient teaching and
+          learning process according to the needs of learning{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/component/page/Home.jsx
+++ src/component/page/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "../NavBar";
 import Feed from "../feed";
+import About from "../About";
 
 const Home = () => {
   return (
     <div className="h-screen w-screen px-4 py-3">
       <NavBar />
-
       <Feed />
+      <About />
     </div>
   );
 };
